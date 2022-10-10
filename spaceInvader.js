c0dc06d2ad66422895fde5376624b7dc8081b1dc
DanielBraga1 <daniel.braga@student.ehb.be>
2022-10-10 11:25:24 +0200

    made space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -11,17 +11,32 @@ drawbox();
 function drawbox() {
     context.fillStyle = "black";
     context.beginPath();
-    context.rect(50, 50, 300, 300);
+    context.rect(0, 0, 300, 300);
 context.fill();    
 
 context.stroke();
 context.strokeStyle = "green";
 context.fillStyle = "green";
 context.beginPath();
-    context.rect(100, 100, 200, 50);
-    context.rect(100, 175, 200, 100);
-    context.rect(135, 275, 50, 50);
-    context.rect(210, 275, 50, 50);
+    context.rect(50, 50, 50, 50);
+    context.rect(100, 50, 50, 50);
+    context.rect(150, 50, 50, 50);
+    context.rect(200, 50, 50, 50);
+
+    context.rect(50, 150, 50, 50);
+    context.rect(100, 150, 50, 50);
+    context.rect(150, 150, 50, 50);
+    context.rect(200, 150, 50, 50);
+
+    context.rect(50, 200, 50, 50);
+    context.rect(100, 200, 50, 50);
+    context.rect(150, 200, 50, 50);
+    context.rect(200, 200, 50, 50);
+
+    context.rect(90, 250, 50, 50);
+    context.rect(170, 250, 50, 50);
+
+
 context.fill();    
 
 
